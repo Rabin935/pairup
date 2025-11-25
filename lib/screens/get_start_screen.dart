18c825314b1fa65b968f2bccf545a091e36696d3
Rabin Tamang <lrabin935@gmail.com>
2025-11-25 22:22:32 +0545

    getstart page created

---
+++ lib/screens/get_start_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class GetStartScreen extends StatefulWidget {
+  const GetStartScreen({super.key});
+
+  @override
+  State<GetStartScreen> createState() => _GetStartScreenState();
+}
+
+class _GetStartScreenState extends State<GetStartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.purple],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+
+        child: Column(
+          children: [
+            const Spacer(flex: 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.people_alt, color: Colors.white, size: 60),
+
+                const SizedBox(width: 15),
+                const Text(
+                  "PairUp",
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(flex: 3),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+
+                  child: const Text(
+                    "Get Started",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                  children: [
+                    TextSpan(text: 'By signing, you can agree to our '),
+                    TextSpan(
+                      text: "Terms.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    TextSpan(text: "\nSee how we use your data in our "),
+                    TextSpan(
+                      text: "Privacy Policy.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
