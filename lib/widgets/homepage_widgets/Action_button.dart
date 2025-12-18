c724550a73a745748668835c5f1c78299cb90106
Rabin Tamang <lrabin935@gmail.com>
2025-12-18 22:05:13 +0545

    added like fav cross button in homepage

--- lib/widgets/homepage_widgets/Action_button.dart
+++ lib/widgets/homepage_widgets/Action_button.dart
@@ -5,6 +5,42 @@ class ActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildCircleButton(Icons.close, Colors.orange, 60),
+        _buildCircleButton(
+          Icons.favorite,
+          Colors.white,
+          80,
+          bgColor: const Color(0xFF7F3DDB),
+        ),
+        _buildCircleButton(Icons.star, Colors.purple, 60),
+      ],
+    );
+  }
+
+  Widget _buildCircleButton(
+    IconData icon,
+    Color color,
+    double size, {
+    Color bgColor = Colors.white,
+  }) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: bgColor,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Icon(icon, color: color, size: size * 0.4),
+    );
   }
 }
