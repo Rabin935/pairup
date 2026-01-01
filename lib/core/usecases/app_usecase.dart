b2d160e82d2909ccf492904bdc65409a39181c67
Rabin Tamang <lrabin935@gmail.com>
2026-01-01 18:36:15 +0545

    core: created usecasewithparams and usecasewithoutparams in app_usecases

---
+++ lib/core/usecases/app_usecase.dart
@@ -0,0 +1,10 @@
+import 'package:either_dart/either.dart';
+import 'package:pairup/core/error/failures.dart';
+
+abstract interface class UsecaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+abstract interface class UsecaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
