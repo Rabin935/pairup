5db82ec4153fee0f6614ebfee2388e5eb82a9baa
Rabin Tamang <lrabin935@gmail.com>
2026-01-31 21:21:07 +0545

    tested first and last name

--- test/screen/signup_test.dart
+++ test/screen/signup_test.dart
@@ -9,4 +9,19 @@ void main() {
     Finder title = find.text('Join Us Today');
     expect(title, findsOneWidget);
   });
+
+  testWidgets('Testing first and last name', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: SignupScreen()));
+
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byType(TextField).first, 'Ram');
+    await tester.enterText(find.byType(TextField).last, 'Tamang');
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Create Account'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Ram'), findsOneWidget);
+    expect(find.text('Tamang'), findsOneWidget);
+  });
 }
